3a8da33cddc9001f58b6b780436a9abfbed9edf3
Apple <>
2021-01-29 20:54:50 +0800

    Swift 新增拼音管理

---
+++ Book/Book/Swift/PinyinManager.swift
@@ -0,0 +1,21 @@
+//
+//  PinyinManager.swift
+//  Book
+//
+//  Created by Apple on 2021/1/29.
+//  Copyright © 2021 马大哈. All rights reserved.
+//
+
+import UIKit
+
+class PinyinManager: NSObject {
+
+    static let sharedInstance = PinyinManager();
+
+    private override init() {
+        
+        super.init()
+        
+    }
+    
+}
